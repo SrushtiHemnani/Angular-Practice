72e36968caa6df10c7385ff033a06bf6a7e363ca
Srushti Hemnani <121381289+SrushtiHemnani@users.noreply.github.com>
2024-04-23 14:13:16 +0530

    minor

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <p>
-      Hello World 
+      Hello Appcarry
     </p>
   `,
   styleUrl: './housing-location.component.css'
